e2eac6ff8049e80ee5d1932b24d1c3f689fc4507
a000000000aaa000a0 <ktq2390326@stu.o-hara.ac.jp>
2024-11-08 14:34:28 +0900

    画面遷移

--- cookapp/urls.py
+++ cookapp/urls.py
@@ -4,4 +4,5 @@ from . import  views
 app_name = "cookapp"
 urlpatterns = [
     path('index/', views.IndexView.as_view(), name='index'),
+    path('home/', views.HomeView.as_view(), name='home')
 ]
--- cookapp/views.py
+++ cookapp/views.py
@@ -7,3 +7,6 @@ from django.views.generic.base import TemplateView
 class IndexView(TemplateView):
     
     template_name='index.html'
+
+class HomeView(TemplateView):
+    template_name='home.html'
