bf9278bdb4a7934dcb2314f9d94bf8fe9a46a39f
a000000000aaa000a0 <ktq2390326@stu.o-hara.ac.jp>
2024-11-08 12:24:40 +0900

    ログアウト

--- account/views.py
+++ account/views.py
@@ -7,10 +7,22 @@ from django.views.generic.base import TemplateView
 
 
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
 from django.urls import reverse_lazy
 
 class CustomLoginView(LoginView):
     template_name = 'login.html'  # ログインページのテンプレート
+    redirect_authenticated_user = True  # ログイン済みユーザーをリダイレクト
+    success_url = reverse_lazy('cookapp:index')  # ログイン後のリダイレクト先
+    
+
+    def get_success_url(self):
+        # カスタムリダイレクト先を指定
+        return self.success_url
+    
+
+class CustomLogoutView(LogoutView):
+    template_name = 'logout.html'  # ログインページのテンプレート
     redirect_authenticated_user = True  # ログイン済みユーザーをリダイレクト
     success_url = reverse_lazy('cookapp:index')  # ログイン後のリダイレクト先
     
