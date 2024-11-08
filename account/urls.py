a605c4b06a96fe23d527ebfdd5b6af0afa22f535
a000000000aaa000a0 <ktq2390326@stu.o-hara.ac.jp>
2024-11-08 12:09:16 +0900

    ログアウト（未完成）

--- account/urls.py
+++ account/urls.py
@@ -5,6 +5,6 @@ from django.contrib.auth import views as auth_views
 app_name = "account"
 urlpatterns = [
     path('login/',  views.CustomLoginView.as_view(), name='login'),
-    path('logout/', views.CustomLogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'),name='logout'),
     path('', views.IndexView.as_view(), name='top'),
 ]
--- account/views.py
+++ account/views.py
@@ -7,7 +7,6 @@ from django.views.generic.base import TemplateView
 
 
 from django.contrib.auth.views import LoginView
-from django.contrib.auth.views import LogoutView
 from django.urls import reverse_lazy
 
 class CustomLoginView(LoginView):
@@ -20,14 +19,6 @@ class CustomLoginView(LoginView):
         # カスタムリダイレクト先を指定
         return self.success_url
     
-class CustomLogoutView(LogoutView):
-    template_name = 'logout.html' # ログアウトページのテンプレート
-    redirect_authenticated_user = True
-    success_url = reverse_lazy('cookapp:index')  # ログアウト後のリダイレクト先
-
-    def get_success_url(self):
-        # カスタムリダイレクト先を指定
-        return self.success_url
 class IndexView(TemplateView):
     
     template_name='top.html'
