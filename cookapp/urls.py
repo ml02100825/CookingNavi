ee237040a1f50fe7c889cd7f681d85620e33f397
a000000000aaa000a0 <ktq2390326@stu.o-hara.ac.jp>
2024-11-11 10:14:32 +0900

    画面遷移

--- cookapp/urls.py
+++ cookapp/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('index/', views.IndexView.as_view(), name='index'),
     path('home/', views.HomeView.as_view(), name='home'),
     path('health_management_main/', views.HealthMainView.as_view(), name='health_management_main'),
-    path('health_selection/', views.HealthSelectionView.as_view(), name='health_selection')
+    path('health_selection/', views.HealthSelectionView.as_view(), name='health_selection'),
+    path('account_setting/', views.AccountSettingView.as_view(), name='account_setting')
 ]
