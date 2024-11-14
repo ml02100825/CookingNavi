0765dacdc6cc8ea7411c74afcff23987ffa1af68
nina <ktq2390316@stu.o-hara.ac.jp>
2024-11-14 12:18:07 +0900

    q

--- account/urls.py
+++ account/urls.py
@@ -8,10 +8,5 @@ urlpatterns = [
     path('signup2/', views.SignUpPage2View.as_view(), name='signup2'),
     path('signup_completion/', views.CustomSignUpView.as_view(), name='signup_completion'),
     path('logout/', views.CustomLogoutView.as_view(), name='logout'),
-    path('username/', views.UsernameView.as_view(), name='username'),
-    path('email/', views.EmailView.as_view(), name='email'),
-    path('email_henko_ok/', views.EmailOkView.as_view(), name='email_henko_ok'),
-    path('password/', views.PasswordView.as_view(), name='password'),
-    path('password_henko_ok/', views.PasswordOkView.as_view(), name='password_henko_ok'),
     path('', views.IndexView.as_view(), name='top'),
 ]
--- cookapp/views.py
+++ cookapp/views.py
@@ -1,15 +1,14 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from .forms import EmailForm, UsernameForm, PasswordForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.base import TemplateView
+from django.contrib import messages
 
 
 class IndexView(TemplateView):
-    
     template_name='index.html'
 
 class CustomLogin1View(TemplateView):
-
     template_name='Login1.html'
 
 class HomeView(TemplateView):
@@ -38,3 +37,78 @@ class NotificationSettingView(TemplateView):
 
 class SubscriptionSettingView(TemplateView):
     template_name='sabusuku/setting/sabusuku_setting.html'
+
+
+class UsernameView(LoginRequiredMixin, TemplateView):
+    template_name = 'acount/name/username_henko.html'
+    def get(self, request, *args, **kwargs):
+        form = UsernameForm()
+        return render(request, self.template_name, {'form': form})
+    
+    def post(self, request, *args, **kwargs):
+        form = UsernameForm(request.POST)
+        if form.is_valid():
+            new_username = form.cleaned_data['new_username']
+            
+            # メールアドレスの更新
+            user = request.user
+            user.Name = new_username
+            user.save()
+
+            messages.success(request, 'ユーザー名が正常に変更されました。')
+            return redirect('cookapp:username_henko_ok')  # プロフィールページなどにリダイレクト
+        
+        return render(request, self.template_name, {'form': form})
+
+class UsernameOkView(TemplateView):
+    template_name = "acount/name/username_henko_ok.html"
+
+
+class EmailView(LoginRequiredMixin, TemplateView):
+    template_name = 'acount/email/email_henko.html'
+    def get(self, request, *args, **kwargs):
+        form = EmailForm()
+        return render(request, self.template_name, {'form': form})
+    
+    def post(self, request, *args, **kwargs):
+        form = EmailForm(request.POST)
+        if form.is_valid():
+            new_email = form.cleaned_data['new_email']
+            
+            # メールアドレスの更新
+            user = request.user
+            user.email = new_email
+            user.save()
+
+            messages.success(request, 'メールアドレスが正常に変更されました。')
+            return redirect('cookapp:email_henko_ok')  # プロフィールページなどにリダイレクト
+        
+        return render(request, self.template_name, {'form': form})
+
+class EmailOkView(TemplateView):
+    template_name = 'acount/email/email_henko_ok.html'
+
+
+class PasswordView(LoginRequiredMixin, TemplateView):
+    template_name = 'acount/password/password_henko.html'
+    def get(self, request, *args, **kwargs):
+        form = PasswordForm()
+        return render(request, self.template_name, {'form': form})
+    
+    def post(self, request, *args, **kwargs):
+        form = PasswordForm(request.POST)
+        if form.is_valid():
+            new_password = form.cleaned_data['new_password']
+            
+            # パスワードの更新
+            user = request.user
+            user.password = new_password
+            user.save()
+
+            messages.success(request, 'パスワードが正常に変更されました。')
+            return redirect('cookapp:password_henko_ok')  # プロフィールページなどにリダイレクト
+        
+        return render(request, self.template_name, {'form': form})
+
+class PasswordOkView(TemplateView):
+    template_name = 'acount/password/password_henko_ok.html'
