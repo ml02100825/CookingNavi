100d2a30cdb04a402bfe344e4febc8fda75343aa
Sago0424 <ktq2390314@stu.o-hara.ac.jp>
2024-11-21 11:53:44 +0900

    uho

---
+++ bbs/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+app_name = "bbs"
+
+urlpatterns = [
+    
+    path('BulletinBoard/', views.BulletinBoardView.as_view(), name='BulletinBoard'),
+]
