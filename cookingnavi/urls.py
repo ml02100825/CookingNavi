313942c88c50ff67c51345a0845b03ca0783583b
a000000000aaa000a0 <ktq2390326@stu.o-hara.ac.jp>
2024-11-22 15:16:33 +0900

    a

--- cookingnavi/urls.py
+++ cookingnavi/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path("", include('account.urls')),
     path("administrator/", include('administrator.urls')),
     path("bbs/",include('bbs.urls'))
-]
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
