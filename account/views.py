6fc6ebf0e9851ddeb0b78e949de7d6a23609913e
a000000000aaa000a0 <ktq2390326@stu.o-hara.ac.jp>
2025-02-07 11:27:39 +0900

    a

--- account/views.py
+++ account/views.py
@@ -100,28 +100,28 @@ class SignUpPage1View(TemplateView):
 
 class SignUpPage2View(TemplateView):
     template_name = 'administrator/sign up/sign up2.html'
- 
+
     def get(self, request, *args, **kwargs):
         form = CustomUserCreation2Form()
         return render(request, self.template_name, {'form': form})
- 
+
     def post(self, request, *args, **kwargs):
         form = CustomUserCreation2Form(request.POST)
         if form.is_valid():
             email = request.session.get('email')
             password = request.session.get('password1')
- 
+
             try:
                 # 既存ユーザーを取得
                 user = User.objects.get(email=email)
- 
+
                 # 非アクティブまたは論理削除ユーザーの場合、復活処理
                 if not user.is_active or user.deleteflag:
                     user.is_active = 1  # アクティブ化
                     user.deleteflag = 0  # deleteflagを0に設定
                     user.set_password(password)  # 新しいパスワードを保存
                     logging.debug(f"非アクティブまたは削除ユーザー {email} を復活しました。")
- 
+
                     # 必要なフィールドの更新
                     user.name = form.cleaned_data['name']
                     user.age = form.cleaned_data['birthdate']
@@ -129,10 +129,10 @@ class SignUpPage2View(TemplateView):
                     user.height = form.cleaned_data['height']
                     user.weight = form.cleaned_data['weight']
                     user.save()
- 
+
                 else:
                     raise User.DoesNotExist  # 通常の新規作成へ進む
- 
+
             except User.DoesNotExist:
                 # 新規ユーザー作成
                 hashed_password = make_password(password)
@@ -146,21 +146,18 @@ class SignUpPage2View(TemplateView):
                     weight=form.cleaned_data['weight'],
                 )
                 user.save()
-            # 生年月日から年齢を計算
-            birthdate = form.cleaned_data['birthdate']
-            today = datetime.today().date()
-            age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
+
             # familymemberテーブルに登録
             family_member = Familymember(
                 family_name=form.cleaned_data['name'],
                 family_gender=form.cleaned_data['gender'],
-                family_age=age,
+                family_age=form.cleaned_data['birthdate'],  # Store birthdate here
                 family_height=form.cleaned_data['height'],
                 family_weight=form.cleaned_data['weight'],
                 user=user  # userオブジェクトを使用
             )
             family_member.save()
- 
+
             # weightテーブルに登録
             weight_entry = Weight(
                 weight=form.cleaned_data['weight'],
@@ -169,18 +166,18 @@ class SignUpPage2View(TemplateView):
                 family=family_member  # familyオブジェクトを使用
             )
             weight_entry.save()
- 
+
             # アレルギー情報の登録または更新
             allergies = form.cleaned_data['allergies']
             for allergy in allergies:
                 Userallergy.objects.update_or_create(
                     user=user, allergy=allergy
                 )
- 
+
             # ログイン処理
             login(request, user)
             return redirect('account:signup_completion')  # 完了画面へ遷移
- 
+
         # フォームが無効な場合
         return render(request, self.template_name, {'form': form})
     
