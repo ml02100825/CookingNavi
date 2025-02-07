e1ef7cbef11a5f0ce2274cbb5d98d6cf1efc1551
ml02100825 <ktq2390315@stu.o-hara.ac.jp>
2025-02-07 09:46:26 +0900

    a

--- cookingnavi/settings.py
+++ cookingnavi/settings.py
@@ -20,9 +20,7 @@ from django.urls import reverse_lazy
  
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-PROJECT_NAME = os.path.basename(BASE_DIR)
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+ 
  
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -192,8 +190,8 @@ else:
         filemode = 'a'
     )
    
-# STATIC_URL = "/static/"
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+STATIC_URL = "/static/"
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 AUTH_USER_MODEL = 'account.User'
  
 MEDIA_URL = '/media/'
